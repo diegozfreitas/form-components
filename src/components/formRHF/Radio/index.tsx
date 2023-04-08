803504cf214c25ee89a3a168403213f287bf2a01
Diego ZF <ddzadravec@gmail.com>
2023-04-08 16:18:20 -0300

    feat: Add error

--- src/components/formRHF/Radio/index.tsx
+++ src/components/formRHF/Radio/index.tsx
@@ -10,6 +10,7 @@ type Props = UseControllerProps & {
   options: CheckboxOption[];
   singleSelect?: boolean;
   label: string;
+  error?: any;
 };
 
 export const Radio: React.FC<Props> = ({
@@ -20,60 +21,65 @@ export const Radio: React.FC<Props> = ({
   singleSelect = false,
   rules,
   label,
+  error,
   ...rest
 }) => {
   const isSingleSelect = singleSelect || options.length === 1;
 
   return (
-    <Controller
-      name={name}
-      control={control}
-      defaultValue={defaultValue}
-      rules={rules}
-      render={({ field: { onChange, onBlur, value } }) => (
-        <>
-          <span>{label}</span>
-          <div role="group" aria-labelledby={`${name}-label`} {...rest}>
-            {options.map((option) => {
-              const handleChange = () => {
-                if (isSingleSelect) {
-                  onChange(option.value);
-                } else {
-                  const values = Array.isArray(value) ? value : [value];
-                  const index = values.indexOf(option.value);
-                  if (index === -1) {
-                    onChange([...values, option.value]);
+    <>
+      <Controller
+        name={name}
+        control={control}
+        defaultValue={defaultValue}
+        rules={rules}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <>
+            <span>{label}</span>
+            <div role="group" aria-labelledby={`${name}-label`} {...rest}>
+              {options.map((option) => {
+                const handleChange = () => {
+                  if (isSingleSelect) {
+                    onChange(option.value);
                   } else {
-                    values.splice(index, 1);
-                    onChange(values);
+                    const values = Array.isArray(value) ? value : [value];
+                    const index = values.indexOf(option.value);
+                    if (index === -1) {
+                      onChange([...values, option.value]);
+                    } else {
+                      values.splice(index, 1);
+                      onChange(values);
+                    }
                   }
-                }
-              };
+                };
 
-              return (
-                <div key={option.value}>
-                  <label htmlFor={`${name}-${option.value}`}>
-                    <input
-                      type="checkbox"
-                      id={`${name}-${option.value}`}
-                      name={name}
-                      value={option.value}
-                      checked={
-                        isSingleSelect
-                          ? value === option.value
-                          : Array.isArray(value) && value.includes(option.value)
-                      }
-                      onChange={handleChange}
-                      onBlur={onBlur}
-                    />
-                    {option.label}
-                  </label>
-                </div>
-              );
-            })}
-          </div>
-        </>
-      )}
-    />
+                return (
+                  <div key={option.value}>
+                    <label htmlFor={`${name}-${option.value}`}>
+                      <input
+                        type="checkbox"
+                        id={`${name}-${option.value}`}
+                        name={name}
+                        value={option.value}
+                        checked={
+                          isSingleSelect
+                            ? value === option.value
+                            : Array.isArray(value) &&
+                              value.includes(option.value)
+                        }
+                        onChange={handleChange}
+                        onBlur={onBlur}
+                      />
+                      {option.label}
+                    </label>
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        )}
+      />
+      {error && <span style={{ color: "red" }}>{error}</span>}
+    </>
   );
 };
